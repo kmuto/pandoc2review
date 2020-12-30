d25991a6d25fd1b36b0541a51bb374071c079fdb
atusy <30277794+atusy@users.noreply.github.com>
2020-12-30 09:29:59 +0900

    revert 370909a

--- nestedlist.lua
+++ nestedlist.lua
@@ -1,20 +1,20 @@
 local beginchild = {pandoc.Plain(pandoc.Str('//beginchild'))}
 local endchild = {pandoc.Plain(pandoc.Str('//endchild'))}
 
-local function itemize(tag, content)
-  pandoc[tag == "BulletList" and "OrderedList" or "BulletList"](content)
-end
-
 local function nestablelist(elem)
-  --[[
-    If a list nests blocks, wrap them with special list items,
-    i.e. `//beginchild` and `//endchild`.
-    Subsequently, `review.lua` turns the items into commands.
-  ]]
   for _, block in ipairs(elem.content) do
     if block[2] then
-      table.insert(block, 2, itemize(block[2].tag, beginchild))
-      table.insert(block, itemize(block[#block].tag, endchild))
+      if block[2].tag == "BulletList" then
+        table.insert(block[2].content, 1, beginchild)
+      else
+        table.insert(block, 2, pandoc.BulletList(beginchild))
+      end
+
+      if block[#block].tag == "BulletList" then
+        table.insert(block[#block].content, endchild)
+      else
+        table.insert(block, pandoc.BulletList(endchild))
+      end
     end
   end
   return elem
@@ -23,5 +23,5 @@ end
 return {
   {BulletList = nestablelist},
   {OrderedList = nestablelist},
-  {DefinitionList = nestablelist}
+  {DefinitionList = nestablelist},
 }
