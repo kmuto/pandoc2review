6f7084ec62cf3bf7e8a2753176700383fb3555f6
Kenshi Muto <kmuto@debian.org>
2021-01-01 17:35:37 +0900

    test for block spec

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,17 @@
 require 'test/unit'
 require 'fileutils'
 require 'open3'
+
+def pandoc(src, opts: nil, err: nil)
+  args = 'pandoc -t review.lua --lua-filter=nestedlist.lua --lua-filter=strong.lua -f markdown-auto_identifiers-smart+east_asian_line_breaks'
+  if opts
+    args += ' ' + opts
+  end
+  if err
+    stdout, stderr, status = Open3.capture3(args, stdin_data: src)
+    return softbreak(stdout), stderr
+  else
+    stdout, status = Open3.capture2(args, stdin_data: src)
+    softbreak(stdout)
+  end
+end
--- test/test_reviewlua.rb
+++ test/test_reviewlua.rb
@@ -2,20 +2,6 @@
 require 'test_helper'
 
 class ReviewLuaTest < Test::Unit::TestCase
-  def pandoc(src, opts: nil, err: nil)
-    args = 'pandoc -t review.lua --lua-filter=nestedlist.lua --lua-filter=strong.lua -f markdown-auto_identifiers-smart+east_asian_line_breaks'
-    if opts
-      args += ' ' + opts
-    end
-    if err
-      stdout, stderr, status = Open3.capture3(args, stdin_data: src)
-      return softbreak(stdout), stderr
-    else
-      stdout, status = Open3.capture2(args, stdin_data: src)
-      softbreak(stdout)
-    end
-  end
-
   def test_para
     src = 'one'
     assert_equal "one\n", pandoc(src)
